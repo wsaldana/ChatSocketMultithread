2d70a56457099c72ea6c8fa430f4bc60f1d425bc
wsaldana <sal19897@uvg.edu.gt>
2022-05-03 10:01:08 -0600

    No multithread

---
+++ client1.c
@@ -0,0 +1,77 @@
+/*
+filename server_ipaddress portno
+
+argv[0] filename
+argv[1] server_ipaddress
+argv[2] portno
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+void error(const char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+int main(int argc, char *argv[])
+{
+    int sockfd, portno, n;
+    struct sockaddr_in server_addr;
+    struct hostent *server;
+
+    char buffer[256];
+    if (argc < 3){
+        fprintf(stderr, "Usage %s hostname port\n", argv[0]);
+        exit(1);
+    }
+    portno = atoi(argv[2]);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if(sockfd < 0)
+    {
+        error("Error opening socket.");
+    }
+
+        server = gethostbyname(argv[1]);
+        if(server == NULL)
+        {
+            fprintf(stderr, "Error, no such host");
+        }
+
+        bzero((char *)&server_addr, sizeof(server_addr));
+        server_addr.sin_family = AF_INET;
+        bcopy((char *) server->h_addr, (char *) &server_addr.sin_addr.s_addr, server->h_length);
+        server_addr.sin_port = htons(portno);
+        if(connect(sockfd, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0)
+            error("Connection Failed");
+
+        while(1)
+        {
+            bzero(buffer, 255);
+            fgets(buffer, 255, stdin);
+            n = write(sockfd, buffer, strlen(buffer));
+            if(n < 0)
+                error("Error on writing");
+
+            bzero(buffer, 255);
+            n = read(sockfd, buffer, 255);
+            if(n < 0)
+                error("Error on reading");
+
+            
+            printf("Server: %s", buffer);
+            
+            int i = strcmp("Bye", buffer);
+            if(i == 0)
+                break;
+        }
+    close(sockfd);
+    return 0;
+}
---
+++ server1.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+
+void error(const char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+int main(int arc, char *argv[])
+{
+    if(arc< 2)
+    {
+        fprintf(stderr, "Port Nomber Not Provided. Program terminated\n");
+        exit(1);
+    }
+    int sockfd, newsockfd, portno, n;
+    char buffer[255];
+
+    struct sockaddr_in server_addr, cli_addr;
+    socklen_t clilen;
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if(sockfd < 0)
+    {
+        error("Error opening socket.");
+    }
+
+    bzero((char *) &server_addr, sizeof(server_addr));
+
+    portno = atoi(argv[1]);
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(portno);
+
+    if(bind(sockfd, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0)
+        error("Binding Failed.");
+    
+    listen(sockfd, 5);
+    clilen = sizeof(cli_addr);
+
+    newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+
+    if(newsockfd < 0)
+        error("Error on Accept");
+
+    while(1)
+    {
+        bzero(buffer, 256);
+        n = read(newsockfd, buffer, 255);
+        if(n < 0)
+            error("Error on reading");
+
+        printf("Client: %s\n", buffer);
+        bzero(buffer, 255);
+        // fgets(buffer, 255, stdin);
+        char msg[5] = "Hola\n";
+        (void) strncpy(buffer, msg, sizeof(msg));
+
+        n = write(newsockfd, buffer, strlen(buffer));
+        if(n < 0)
+            error("Error on writing");
+
+        int i = strcmp("Bye", buffer);
+        if(i == 0)
+        break;
+    }
+    close(newsockfd);
+    close(sockfd);
+    return 0;
+}
