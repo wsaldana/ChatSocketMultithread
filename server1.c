4da69e4947e91a146f75818a60b58611f1b95855
wsaldana <sal19897@uvg.edu.gt>
2022-05-03 12:54:22 -0600

    Multithread server msgs

--- server1.c
+++ server1.c
@@ -5,30 +5,59 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <pthread.h>
 
+// Requests buffer
+char buffer[256];
 
-void error(const char *msg)
-{
+// Socket
+int sockfd, newsockfd, n;
+
+void error(const char *msg){
     perror(msg);
     exit(1);
 }
 
-int main(int arc, char *argv[])
-{
-    if(arc< 2)
-    {
+void* serverthread(void* args){
+    while(1){
+        // Read from client
+        bzero(buffer, 256);
+        n = read(newsockfd, buffer, 255);
+        if(n < 0)
+            error("Error on reading");
+        printf("Client: %s\n", buffer);
+
+        // send to client
+        bzero(buffer, 255);
+        // fgets(buffer, 255, stdin);
+        char msg[5] = "Hola\n";
+        (void) strncpy(buffer, msg, sizeof(msg));
+
+        n = write(newsockfd, buffer, strlen(buffer));
+        if(n < 0)
+            error("Error on writing");
+    }
+
+    // Close thread
+    pthread_exit(NULL);
+    return 0;
+}
+
+int main(int arc, char *argv[]){
+    pthread_t tid;
+
+    if(arc< 2){
         fprintf(stderr, "Port Nomber Not Provided. Program terminated\n");
         exit(1);
     }
-    int sockfd, newsockfd, portno, n;
+    int portno, n;
     char buffer[255];
 
     struct sockaddr_in server_addr, cli_addr;
     socklen_t clilen;
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if(sockfd < 0)
-    {
+    if(sockfd < 0){
         error("Error opening socket.");
     }
 
@@ -51,27 +80,9 @@ int main(int arc, char *argv[])
     if(newsockfd < 0)
         error("Error on Accept");
 
-    while(1)
-    {
-        bzero(buffer, 256);
-        n = read(newsockfd, buffer, 255);
-        if(n < 0)
-            error("Error on reading");
+    pthread_create(&tid, NULL, serverthread, NULL);
 
-        printf("Client: %s\n", buffer);
-        bzero(buffer, 255);
-        // fgets(buffer, 255, stdin);
-        char msg[5] = "Hola\n";
-        (void) strncpy(buffer, msg, sizeof(msg));
-
-        n = write(newsockfd, buffer, strlen(buffer));
-        if(n < 0)
-            error("Error on writing");
-
-        int i = strcmp("Bye", buffer);
-        if(i == 0)
-        break;
-    }
+    pthread_join(tid, NULL);
     close(newsockfd);
     close(sockfd);
     return 0;
