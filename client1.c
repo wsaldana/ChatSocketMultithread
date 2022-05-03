277e56468cac85d9d0d515d49c6e17895d0e1a03
JavierCotto5201 <53280525+JavierCotto5201@users.noreply.github.com>
2022-05-03 12:25:49 -0600

    Update client1.c
    JSON createad

--- client1.c
+++ client1.c
@@ -8,8 +8,18 @@
 #include <netdb.h>
 #include <pthread.h>
 
+//JSON Objects
+#include <json-c/json.h>
+
+//Actual Date library
+#include <time.h>
+
+char *name;
 // Requests buffer
-char buffer[256];
+char buffer[1024];
+
+//Request Instruction
+char *requestI;
 
 // Socket
 int sockfd, n;
@@ -21,17 +31,104 @@ void error(const char *msg){
 
 void* clienthread(void* args){
     int client_request = *((int*)args);
-
+    char privName;
+    char status;
     // Send to server
     bzero(buffer, 255);
     // fgets(buffer, 255, stdin);
+    
     if(client_request==1){
-        char msg[5] = "aaa\n";
-        (void) strncpy(buffer, msg, sizeof(msg));
+        //Request GET_CHAT
+    	struct json_object *general_chat = json_object_new_object();
+    	json_object_object_add(general_chat, "request", json_object_new_string("GET_CHAT"));
+    	json_object_object_add(general_chat, "body", json_object_new_string("all"));
+    
+    	requestI = json_object_to_json_string_ext(general_chat, JSON_C_TO_STRING_SPACED | JSON_C_TO_STRING_PRETTY);
+        (void) strncpy(buffer, requestI, strlen(requestI));
+        
+    }else if(client_request==2){
+    	printf("Ingrese usuario con el que quiere comunicarse:\t");
+    	scanf("%s", &privName);
+    	
+    	if(strlen(&privName) > 0){
+	    	//Request GET_CHAT
+	    	struct json_object *private_chat = json_object_new_object();
+	    	json_object_object_add(private_chat, "request", json_object_new_string("GET_CHAT"));
+	    	json_object_object_add(private_chat, "body", json_object_new_string(&privName));
+	    	
+	    	requestI = json_object_to_json_string_ext(private_chat, JSON_C_TO_STRING_SPACED | JSON_C_TO_STRING_PRETTY);
+
+		printf("\n\n\t\t\tCHAT PRIVADO\n\n\n");
+		
+		requestI = json_object_to_json_string_ext(private_chat, JSON_C_TO_STRING_SPACED | JSON_C_TO_STRING_PRETTY);
+        	(void) strncpy(buffer, requestI, strlen(requestI));
+        	
+		}
+    }else if(client_request==3){
+	printf("Ingrese el nuevo status (0, Activo), (1, Inactivo), (2, Ocupado):\t");
+	scanf("%s", &status);
+
+	if(strlen(&status) > 0){
+	
+	    	//Request PUT_STATUS
+	    	struct json_object *change_status = json_object_new_object();
+	    	json_object_object_add(change_status, "request", json_object_new_string("PUT_STATUS"));
+		json_object_object_add(change_status, "body", json_object_new_string(&status));
+		    	
+	    	requestI = json_object_to_json_string_ext(change_status, JSON_C_TO_STRING_SPACED | JSON_C_TO_STRING_PRETTY);
+	    	(void) strncpy(buffer, requestI, strlen(requestI));
+		//printf("jobj from str:\n--\n%s\n--\n", requestI);
+		printf("\n\n\t\t\tCAMBIO DE STATUS EN PROCESO...\n\n\n");
+		}
+
+    }else if(client_request == 4){
+    	//Request USER_LIST
+    	struct json_object *user_list = json_object_new_object();
+    	json_object_object_add(user_list, "request", json_object_new_string("GET_USER"));
+    	json_object_object_add(user_list, "body", json_object_new_string("all"));
+    
+    	requestI = json_object_to_json_string_ext(user_list, JSON_C_TO_STRING_SPACED | JSON_C_TO_STRING_PRETTY);
+    	//printf("jobj from str:\n--\n%s\n--\n", requestI);
+    	
+        printf("\n\n\t\t\tLISTA DE USUARIOS CONECTADOS\n\n\n");
+    	(void) strncpy(buffer, requestI, strlen(requestI));
+    
+    }else if(client_request == 5){
+    	printf("Ingrese usuario del que quiere información:\t");
+    	scanf("%s", &privName);
+    	
+    	if(strlen(&privName) > 0){
+	    	//Request USER_INFO
+	    	struct json_object *user_info = json_object_new_object();
+	    	json_object_object_add(user_info, "request", json_object_new_string("GET_USER"));
+	    	json_object_object_add(user_info, "body", json_object_new_string(&privName));
+	    	
+	    	requestI = json_object_to_json_string_ext(user_info, JSON_C_TO_STRING_SPACED | JSON_C_TO_STRING_PRETTY);
+	    	(void) strncpy(buffer, requestI, strlen(requestI));
+	    	//printf("jobj from str:\n--\n%s\n--\n", requestI);
+
+		printf("\n\n\t\t\tINFORMACIÓN DE USUARIO\n\n\n");
+		}
+		
+    }else if(client_request == 6){
+    	printf("\n\n\t\t\tMANUAL DE USO\n\n\n");
+        printf("Holaaa, bienvenido al CHATROOM de Sistemas Operativos\n");
+        printf("Selecciona una opción del Ménu de Opciones desplegado anteriormente\n\n");
+	printf("Chatear con todos los usuarios: Podras chatear con todos los usuarios conectados al chatroom.\n\n");
+	printf("Chat privado: Podras chatear con un usuario en especifico\n\n");
+	printf("Cambiar status: Cambias tu status entre activo(0), inactivo(1), ocupado(2).\n\n");
+	printf("Usuarios conectados: Mostrata la lista de todos los usuarios conectados al servidor.\n\n");
+	printf("Información de usuario: Mostrara la información de un usuario en especifico.\n\n");
+	printf("Desconectarse: Te desconectaras del chatroom.\n\n");
+	
     }else{
-        char msg[5] = "bbb\n";
-        (void) strncpy(buffer, msg, sizeof(msg));
+    	//Request ENDING CONNECTION
+    	struct json_object *end_connection = json_object_new_object();
+    	json_object_object_add(end_connection, "request", json_object_new_string("END_CONEX")); 
+    	requestI = json_object_to_json_string_ext(end_connection, JSON_C_TO_STRING_SPACED | JSON_C_TO_STRING_PRETTY);
+    	(void) strncpy(buffer, requestI, strlen(requestI));
     }
+    
     n = write(sockfd, buffer, strlen(buffer));
     if(n < 0)
         error("Error on writing");
@@ -59,7 +156,7 @@ int main(int argc, char *argv[]){
         exit(1);
     }
 
-    char *name = (argv[3]);
+    name = (argv[3]);
 
     portno = atoi(argv[2]);
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -79,6 +176,26 @@ int main(int argc, char *argv[]){
     if(connect(sockfd, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0)
         error("Connection Failed");
 
+    //Obtener DATE
+    time_t t = time(NULL);
+    time(&t);
+    
+    char *connection_date = ctime(&t);
+    
+    //Request Connection
+    struct json_object *init_connection = json_object_new_object();
+    json_object_object_add(init_connection, "request", json_object_new_string("INIT_CONEX"));
+    
+    struct json_object *body = json_object_new_array();
+    json_object_array_add(body, json_object_new_string(name));
+    json_object_array_add(body, json_object_new_string(connection_date));
+    json_object_object_add(init_connection, "body", body);
+    
+    requestI = json_object_to_json_string_ext(init_connection, JSON_C_TO_STRING_SPACED | JSON_C_TO_STRING_PRETTY);
+    (void) strncpy(buffer, requestI, strlen(requestI));
+    
+    n = write(sockfd, buffer, strlen(buffer));
+    
     while(1){
         printf("\n\n\t\tSistemas Operativos Sección 10 Chat\n\n\n");
         printf("1. Chatear con todos los usuarios \n");
